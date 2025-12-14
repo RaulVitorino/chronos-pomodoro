3c32e7a694607a719a7b86df2c4c5374cec2d622
Raul Vitorino <raul-vitorino@hotmail.com>
2025-12-13 21:53:01 -0300

    commit da primeira função

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import AppFuncIncial from "./App";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AppFuncIncial />
+    <h1>Ola mundo</h1>
+  </StrictMode>
+);
